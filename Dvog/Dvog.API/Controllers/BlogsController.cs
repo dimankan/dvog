32831ca0ce90e06fcf6a9e5c9e6fca85e8a5db3c
dimankan <dimankan94@mail.ru>
2022-05-14 09:38:35 +0300

    feat: setup docker compose + styleCop

---
+++ Dvog/Dvog.API/Controllers/BlogsController.cs
@@ -0,0 +1,12 @@
+﻿namespace Dvog.API.Controllers
+{
+    public class BlogsController
+    {
+        private readonly ILogger<BlogsController> _logger;
+
+        public BlogsController(ILogger<BlogsController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
