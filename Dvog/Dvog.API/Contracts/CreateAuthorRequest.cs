e1cc30af0e352573662cace2a2d6fc796bfcdeb0
dimankan <dimankan94@mail.ru>
2022-07-09 07:48:06 +0300

    1) добавил контроллер для автора. 2) тесты под них. 3) Несколько Request/Response. 4) Переделал Author. Убрал лишние поля

---
+++ Dvog/Dvog.API/Contracts/CreateAuthorRequest.cs
@@ -0,0 +1,17 @@
+﻿using Dvog.Domain;
+using System.ComponentModel.DataAnnotations;
+
+namespace Dvog.API.Contracts
+{
+    public class CreateAuthorRequest
+    {
+        [Required]
+        [StringLength(Author.MaxUserName)]
+        public string UserName { get; set; }
+
+        public CreateAuthorRequest(string userName)
+        {
+            UserName = userName;
+        }
+    }
+}
---
+++ Dvog/Dvog.API/Contracts/CreateAuthorResponse.cs
@@ -0,0 +1,14 @@
+﻿using Dvog.Domain;
+using System.ComponentModel.DataAnnotations;
+
+namespace Dvog.API.Contracts
+{
+    public class CreateAuthorResponse
+    {
+        [Required]
+        public string? ErrorMessage { get; set; }
+
+        [Required]
+        public int IdAuthor { get; set; }
+    }
+}
