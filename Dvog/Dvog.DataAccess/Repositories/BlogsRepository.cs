df9c904724bfa9131c117ef12f3b74f9bc945b41
dimankan <dimankan94@mail.ru>
2022-08-19 00:25:17 +0300

    fix: добил реализацию для репозитория блога, в частности для методов Get, Update, Delete

--- Dvog/Dvog.DataAccess/Repositories/BlogsRepository.cs
+++ Dvog/Dvog.DataAccess/Repositories/BlogsRepository.cs
@@ -10,8 +10,6 @@ namespace Dvog.DataAccess.Repositories
     {
         private readonly DvogDbContext _dbContext;
         private readonly IMapper _mapper;
-        private Dictionary<int, Blog> _blogs = new Dictionary<int, Blog>();
-        private int _latestId = 0;
 
         public BlogsRepository(DvogDbContext dbContext, IMapper mapper)
         {
@@ -21,7 +19,7 @@ namespace Dvog.DataAccess.Repositories
 
         public int Add(Blog newBlog)
         {
-            var blog = new Entities.Blog() { Text = newBlog.Text, Title = newBlog.Title};
+            var blog = new Entities.Blog() { Text = newBlog.Text, Title = newBlog.Title };
             _dbContext.Blogs.Add(blog);
             _dbContext.SaveChanges();
 
@@ -34,55 +32,33 @@ namespace Dvog.DataAccess.Repositories
             return _mapper.Map<Entities.Blog[], Blog[]>(blogs);
         }
 
-        public string Get(int idBlog)
+        public Blog Get(int idBlog)
         {
-            string result = string.Empty;
-
-            if (!_blogs.TryGetValue(idBlog, out var blog))
-            {
-                return $"Блога с идентификатором {idBlog} не существует";
-            }
-
-            result += $"\nId: {blog.Id}\nЗаголовок: {blog.Title}.\nТекст: {blog.Text}\n---Конец блога---\n\n";
-
-            return result;
+            var blog = _dbContext.Blogs.SingleOrDefault(b => b.Id == idBlog);
+            return _mapper.Map<Entities.Blog, Blog>(blog);
         }
 
-        public string Update(int idBlog, Result<Blog> updatedBlog)
+        public Blog Update(int idBlog, Result<Blog> updatedBlog)
         {
-            string result = string.Empty;
+            var blog = _dbContext.Blogs.SingleOrDefault(b => b.Id == idBlog);
 
-            //var blog = _blogs[idBlog];
-            if (!_blogs.TryGetValue(idBlog, out var blog))
-            {
-                return $"Блога с идентификатором {idBlog} не существует";
-            }
+            blog.Text = updatedBlog.Value.Text;
+            blog.Title = updatedBlog.Value.Title;
 
-            //var newBlog = Blog.Create(blog.Title + " " + DateTime.Now, blog.Text + " " + DateTime.Now);
-            //newBlog = newBlog.Value with { Id = idBlog };
-            //_blogs[idBlog] = newBlog.Value;
+            _dbContext.Update(blog);
+            _dbContext.SaveChanges();
 
-            // Тут снова Value!!!
-            _blogs[idBlog] = updatedBlog.Value with { Id = blog.Id };
-
-            result = $"Блог с идентификатором {idBlog} обновлен";
-
-            return result;
+            return _mapper.Map<Entities.Blog, Blog>(blog);
         }
 
         public string Delete(int idBlog)
         {
-            string result = string.Empty;
+            var blog = _dbContext.Blogs.SingleOrDefault(b => b.Id == idBlog);
 
-            if (!_blogs.ContainsKey(idBlog))
-            {
-                return $"Блога c идентификатором {idBlog} не существует";
-            }
+            _dbContext.Remove(blog);
+            _dbContext.SaveChanges();
 
-            _blogs.Remove(idBlog);
-            result = $"Блог с идентификатором {idBlog} удалён";
-
-            return result;
+            return $"Delete {blog.Title} with Id = {idBlog}";
         }
     }
 }
