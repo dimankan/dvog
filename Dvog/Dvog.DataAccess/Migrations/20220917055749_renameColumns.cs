8366c175a587fdd242475988a1728125554cee1e
dimankan <dimankan94@mail.ru>
2022-09-17 08:58:45 +0300

    add migration renameColumns

---
+++ Dvog/Dvog.DataAccess/Migrations/20220917055749_renameColumns.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dvog.DataAccess.Migrations
+{
+    public partial class renameColumns : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateLastUpdate",
+                table: "Authors");
+
+            migrationBuilder.DropColumn(
+                name: "DateRegistr",
+                table: "Authors");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "Authors",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastUpdateDate",
+                table: "Authors",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "Authors");
+
+            migrationBuilder.DropColumn(
+                name: "LastUpdateDate",
+                table: "Authors");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateLastUpdate",
+                table: "Authors",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateRegistr",
+                table: "Authors",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
